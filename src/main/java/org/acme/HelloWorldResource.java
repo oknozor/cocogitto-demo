6f5ba46bc879b663d5fdd0394dae6dfdbd33328c
Paul Delafosse <paul.delafosse@protonmail.com>
2022-02-22 10:35:42 +0100

    feat: add feature one

--- src/main/java/org/acme/HelloWorldResource.java
+++ src/main/java/org/acme/HelloWorldResource.java
@@ -8,4 +8,8 @@ import javax.ws.rs.core.MediaType;
 @Path("hello")
 @Produces(MediaType.TEXT_PLAIN)
 public class HelloWorldResource {
+    @GET
+    public String genericHello() {
+        return "Hello stranger";
+    }
 }
