0982c2d0628b41e7bd175cf339f6a7da57aee7d9
Paul Delafosse <paul.delafosse@protonmail.com>
2022-03-02 16:25:57 +0100

    feat(rest): add hello world endpoint

--- src/test/java/org/acme/HelloWorldResourceTest.java
+++ src/test/java/org/acme/HelloWorldResourceTest.java
@@ -8,4 +8,12 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class HelloWorldResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello stranger"));
+    }
 }
