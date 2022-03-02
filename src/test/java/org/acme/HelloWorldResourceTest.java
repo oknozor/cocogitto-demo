16131f4b6959b2dff99b97813257f2935e28b73a
Paul Delafosse <paul.delafosse@protonmail.com>
2022-03-02 11:50:13 +0100

    ci(github): add Release action

--- src/test/java/org/acme/HelloWorldResourceTest.java
+++ src/test/java/org/acme/HelloWorldResourceTest.java
@@ -8,23 +8,4 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class HelloWorldResourceTest {
-
-    @Test
-    void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello stranger"));
-    }
-
-    @Test
-    void testHelloYouEndpoint() {
-        given()
-                .when().get("/hello/tom")
-                .then()
-                .statusCode(200)
-                .body(is("Hello tom"));
-    }
-
 }
